d29a3bb31c4f9eea4a5fcc8dd3ee6e3917b8dcf2
Frances Reagan James <adtgwen@gmail.com>
2025-05-25 22:31:33 -0400

    added main ts code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import { calculateTax } from './utils/taxCalculator';
 import { PhysicalProduct } from "./models/PhysicalProduct";
 import { DigitalProduct } from ".models/DigitalProduct";
+import { calculateTax } from './utils/taxCalculator';
 
-const laptop = new PhysicalProduct("P001", "Laptop", 999.99, 2.5);
-const priceWithTax = calculateTax(laptop);
-console.log(priceWithTax);
+//create some products//
+const laptop = new PhysicalProduct("PHY001", "Gaming Laptop", 1299.99, 2.5);
+const headphones = new PhysicalProduct("PHY002", "Wireless Headphones", 199.99, 0.3);
 
-const Music-and-Audio = new DigitalProduct("DM02", "Music for Sleeping", 14.99,"MusicAudiomusic.app");
-const priceWithTax = calculateTax(laptop);
-console.log(priceWithTax);
+const ebook = new DigitalProduct("DIG001", "TypeScript Guide", 28.99, 15);
+const software = new DigitalProduct("DIG002", "Photo Editor Pro", 109.89, 500);
+
+//put in an array//
+const products = [laptop, headphones, ebook, software];
+
+console.log("=== Inventory Tracker System ===");
+console.log("");
+
+//loop through the products//
+for (let i = 0; i < products.length; i++) {
+  const product = products[i];  
+  console.log(product.displayDetails());
+
+  
+}
