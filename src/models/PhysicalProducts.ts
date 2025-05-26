f2204f252b61cb0c4698ff25690ec7c07ad5a2f6
Frances Reagan James <adtgwen@gmail.com>
2025-05-25 22:10:24 -0400

    added get getWeight and getPricewithTax

--- src/models/PhysicalProducts.ts
+++ src/models/PhysicalProducts.ts
@@ -1,4 +1,4 @@
-import  Product  from "./Product";
+import  Product from './Product'; //not sure why the curly braces that should go around Product get an error//
 
 export default class PhysicalProduct extends Product {
   weight: number;
@@ -8,14 +8,23 @@ export default class PhysicalProduct extends Product {
     this.weight = weight;
   }
 
+  //forgot to add kg//
+  getWeight(): string {
+    return this.weight + "kg";
+  }
+
+  getPriceWithTax(): number {  
+    //googled how to calculate tax//
+    let tax = this.price * 0.1;
+    let total = this.price + tax;
+    return total;
+  }
+
+
   displayDetails(): string {
     return super.displayDetails() + ` It weighs ${this.weight} kg.`;
   }
 
-
- getPriceWithTax(): number {
-    Product.taxRate = 0.10; // Set 10% tax rate
-    return this.price * (Product.taxRate + 1);
 }
 
  get formattedWeight(): string {
